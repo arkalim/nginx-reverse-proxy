ea482ae24cc066ec015149bd8b835c48ce1cef19
Abdur Rahman Kalim <abdur.rahman.kalim@gmail.com>
2022-11-06 21:52:09 -0500

    implemented redirect path correction

--- app/index.js
+++ app/index.js
@@ -3,8 +3,17 @@ const app = express();
 const port = 80;
 
 app.get("/", (req, res) => {
+  res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
   // console.log(req);
-  res.send(`Hello World`);
+  res.send(`Redirected to login`);
+});
+
+app.get("/users", (req, res) => {
+  // console.log(req);
+  res.send(`Users page`);
 });
 
 app.listen(port, () => {
